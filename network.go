9e02c866e49971784042c8dbf8d97a56bf5ddb9d
zvedzochka <102465369+neuroliptica@users.noreply.github.com>
2023-11-09 23:11:20 +0300

    post with multipart data

--- network.go
+++ network.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"time"
 )
@@ -17,35 +18,45 @@ type RequestInternal struct {
 	Response *http.Response
 }
 
-type PostRequest struct {
-	RequestInternal
-	Payload []byte
-}
-
 type GetRequest struct {
 	RequestInternal
+}
+
+type PostRequest struct {
+	RequestInternal
+	Payload *bytes.Buffer
+}
+
+func (internal *RequestInternal) Set(req *http.Request) {
+	for key, value := range internal.Headers {
+		req.Header.Set(key, value)
+	}
+	for i := range internal.Cookies {
+		req.AddCookie(internal.Cookies[i])
+	}
+}
+
+func (internal *RequestInternal) Do(req *http.Request) (*http.Response, error) {
+	client := &http.Client{
+		Timeout: internal.Timeout,
+	}
+	if internal.Transport != nil {
+		client.Transport = internal.Transport
+	}
+	return client.Do(req)
 }
 
 func (preq *PostRequest) Perform() ([]byte, error) {
 	req, err := http.NewRequest(
 		"POST",
 		preq.Url,
-		bytes.NewBuffer(preq.Payload),
+		preq.Payload,
 	)
 	if err != nil {
 		return nil, err
 	}
-	for key, value := range preq.Headers {
-		req.Header.Set(key, value)
-	}
-	for i := range preq.Cookies {
-		req.AddCookie(preq.Cookies[i])
-	}
-	client := &http.Client{
-		Timeout: preq.Timeout,
-	}
-
-	preq.Response, err = client.Do(req)
+	preq.RequestInternal.Set(req)
+	preq.Response, err = preq.RequestInternal.Do(req)
 	if err != nil {
 		return nil, err
 	}
@@ -58,19 +69,63 @@ func (greq *GetRequest) Perform() ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	for key, value := range greq.Headers {
-		req.Header.Set(key, value)
-	}
-	for i := range greq.Cookies {
-		req.AddCookie(greq.Cookies[i])
-	}
-	client := &http.Client{
-		Timeout: greq.Timeout,
-	}
-	greq.Response, err = client.Do(req)
+	greq.RequestInternal.Set(req)
+	greq.Response, err = greq.RequestInternal.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer greq.Response.Body.Close()
 	return ioutil.ReadAll(greq.Response.Body)
+}
+
+type FilesForm struct {
+	Name  string
+	Files map[string][]byte
+}
+
+type PostMultipartRequest struct {
+	Request PostRequest
+	Form    FilesForm
+	Params  map[string]string
+}
+
+func (preq *PostMultipartRequest) Perform() ([]byte, error) {
+	preq.Request.Payload = new(bytes.Buffer)
+	writer := multipart.NewWriter(preq.Request.Payload)
+
+	for key, value := range preq.Params {
+		err := writer.WriteField(key, value)
+		if err != nil {
+			writer.Close()
+			return nil, err
+		}
+	}
+	for file, cont := range preq.Form.Files {
+		part, err := writer.CreateFormFile(preq.Form.Name, file)
+		if err != nil {
+			writer.Close()
+			return nil, err
+		}
+		part.Write(cont)
+	}
+	writer.Close()
+
+	req, err := http.NewRequest(
+		"POST",
+		preq.Request.Url,
+		preq.Request.Payload,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	preq.Request.Headers["Content-Type"] = writer.FormDataContentType()
+	preq.Request.RequestInternal.Set(req)
+	preq.Request.Response, err = preq.Request.RequestInternal.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer preq.Request.Response.Body.Close()
+	return ioutil.ReadAll(preq.Request.Response.Body)
 }
