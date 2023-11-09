cd9725acca8b8b255c856f6621dfcabf715feef2
zvedzochka <102465369+neuroliptica@users.noreply.github.com>
2023-11-09 23:11:20 +0300

    basic env parsing

---
+++ env.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+// Wipe modes.
+const (
+	SingleThread = iota
+	RandomThreads
+	Creating
+)
+
+type PostSettings struct {
+	Board, Thread, Email string
+	// Thread = 0 when creating
+}
+
+type Env struct {
+	WipeMode uint8
+	PostSettings
+
+	Proxies      []string // TODO: proxies type
+	Media, Texts []string
+
+	Errors []error
+}
+
+func (env *Env) GetProxies(path string) *Env {
+	cont, err := os.ReadFile(path)
+	if err != nil {
+		env.Errors = append(env.Errors, err)
+		return env
+	}
+	env.Proxies = strings.Split(string(cont), "\n")
+	return env
+}
+
+func (env *Env) GetMedia(path string) *Env {
+	entry, err := os.ReadDir(path)
+	if err != nil {
+		env.Errors = append(env.Errors, err)
+		return env
+	}
+	exts := []string{
+		".jpg",
+		".png",
+		".jpeg",
+		".webm",
+		".mp4",
+		".gif",
+	}
+	for i := range entry {
+		for j := range exts {
+			if strings.HasSuffix(entry[i].Name(), exts[j]) {
+				env.Media = append(env.Media, entry[i].Name())
+			}
+		}
+	}
+	return env
+}
+
+func (env *Env) GetTexts(path string) *Env {
+	cont, err := os.ReadFile(path)
+	if err != nil {
+		env.Errors = append(env.Errors, err)
+		return env
+	}
+	env.Proxies = strings.Split(string(cont), "\n\n")
+	return env
+}
+
+func (env *Env) ParseWipeMode() *Env {
+	return env
+}
+
+func (env *Env) ParsePostSettings() *Env {
+	return env
+}
+
+func (env *Env) ParsingFailed() bool {
+	return len(env.Errors) != 0
+}
+
+// Env{}.ParseWipeMode().ParsePostSettings().GetProxies().GetMedia().
