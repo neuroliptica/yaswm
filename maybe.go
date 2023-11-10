7136c6dcb7e07831df867852163b44625fda4364
zvedzochka <102465369+neuroliptica@users.noreply.github.com>
2023-11-10 19:46:04 +0300

    helper functions

--- maybe.go
+++ maybe.go
@@ -11,3 +11,36 @@ func (m Maybe) Eval() error {
 	}
 	return nil
 }
+
+func All[T any](list []T, p func(T) bool) bool {
+	for i := range list {
+		if !p(list[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func Any[T any](list []T, p func(T) bool) bool {
+	return !All(list, func(x T) bool {
+		return !p(x)
+	})
+}
+
+func Map[A any, B any](list []A, f func(A) B) []B {
+	l := make([]B, 0)
+	for i := range list {
+		l = append(l, f(list[i]))
+	}
+	return l
+}
+
+func Filter[A any](list []A, f func(A) bool) []A {
+	l := make([]A, 0)
+	for i := range list {
+		if f(list[i]) {
+			l = append(l, list[i])
+		}
+	}
+	return l
+}
